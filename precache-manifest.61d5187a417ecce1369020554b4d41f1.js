bde379ebb2f81c957e724ad66fb5de0d89255664
SJ5779 <46369502+SJ5779@users.noreply.github.com>
2019-12-10 15:28:52 -0800

    Updates

---
+++ precache-manifest.61d5187a417ecce1369020554b4d41f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd3993f6a956ed7e1709fc704595c35",
+    "url": "/SJ5779/NBA.git/index.html"
+  },
+  {
+    "revision": "a883e0b9d4694346aa24",
+    "url": "/SJ5779/NBA.git/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "5dc8aa59fc4b583a27ae",
+    "url": "/SJ5779/NBA.git/static/css/main.cd695efd.chunk.css"
+  },
+  {
+    "revision": "a883e0b9d4694346aa24",
+    "url": "/SJ5779/NBA.git/static/js/2.24ec7092.chunk.js"
+  },
+  {
+    "revision": "200adb52ba43a6717952f5f45e02d6cb",
+    "url": "/SJ5779/NBA.git/static/js/2.24ec7092.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dc8aa59fc4b583a27ae",
+    "url": "/SJ5779/NBA.git/static/js/main.2a782e13.chunk.js"
+  },
+  {
+    "revision": "15ff25b0bc705b771594",
+    "url": "/SJ5779/NBA.git/static/js/runtime-main.2e6a250e.js"
+  },
+  {
+    "revision": "9470e2d5c6c7f425edc8e9d8baebdb05",
+    "url": "/SJ5779/NBA.git/static/media/nba-logoman-word-white.9470e2d5.svg"
+  }
+]);
